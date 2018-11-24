1d42b821078930d7b1bcdab880b425073774d75a
ekarp <emilyrkarp@gmail.com>
2018-11-24 08:23:50 -0500

    mv vue output to 'static'

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, xkcd),
-    assetsSubDirectory: 'media',
+    assetsSubDirectory: 'static',
     assetsPublicPath: '/',  
 
     /**
