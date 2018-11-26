0285007966baaa1ceda511763cd872a8592c43b6
ekarp <emilyrkarp@gmail.com>
2018-11-26 14:04:21 -0500

    infinite scroll

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="app" v-masonry
-    item-selector=".comic"
-    transition-duration="0.3s">
-    <Comic
-      v-for="comic in comics"
-      v-bind:key="comic.xkcd"
-      v-bind:xkcd="comic"
-      v-masonry-tile class="comic"
-    ></Comic>
+  <div id="app">
+    <main v-masonry item-selector=".comic"
+      transition-duration="0.3s">
+      <Comic
+        v-for="comic in comics"
+        v-bind:key="comic.xkcd"
+        v-bind:xkcd="comic"
+        v-masonry-tile class="comic"
+      ></Comic>
+    </main>
+    <sub v-if="moreToLoad">Loading…</sub>
+    <sub v-else>You have finished xkcd.</sub>
   </div>
 </template>
 
@@ -24,13 +27,34 @@ export default {
   components: { Comic },
   data () {
     return {
-      comics: []
+      comics: [],
+      page: 0,
+      moreToLoad: true
+    }
+  },
+  methods: {
+    scroll () {
+      let _this = this
+
+      window.onscroll = () => {
+        let d = document.documentElement
+
+        if (d.scrollHeight - d.scrollTop === d.clientHeight) {
+          axios.get(`http://127.0.0.1:8000/xkcds/?page=${++_this.page}`)
+            .then(response => {
+              if (response.data.comics) {
+                _this.comics = _this.comics.concat(response.data.comics)
+                _this.page = response.data.page
+              } else {
+                _this.moreToLoad = false
+              }
+            })
+        }
+      }
     }
   },
   mounted () {
-    axios
-      .get('http://127.0.0.1:8000/xkcds/')
-      .then(response => (this.comics = response.data))
+    this.scroll()
   }
 }
 </script>
@@ -38,6 +62,8 @@ export default {
 <style>
 body {
   background-color: cornflowerblue;
+  height: calc(100vh + 5px);
+  margin-bottom: 5px;
 }
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -52,5 +78,8 @@ body {
 }
 img {
   max-width: 100%;
+}
+sub {
+  margin: 50px 0;
 }
 </style>
